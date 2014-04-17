c3a59be8bba10d1e4c3e42e246c462a0b7c8acd8
Justin Love <git@JustinLove.name>
2014-04-17 16:08:15 -0500

    qrl

--- lib/sqrl/url.rb
+++ lib/sqrl/url.rb
@@ -4,9 +4,26 @@ require 'uri/https'
 
 module URI
   class SQRL < HTTPS
-    DEFAULT_PORT = 443
+    def self.scheme
+      'sqrl'
+    end
+
+    def post_scheme
+      'https'
+    end
   end
   @@schemes['SQRL'] = SQRL
+
+  class QRL < HTTP
+    def self.scheme
+      'qrl'
+    end
+
+    def post_scheme
+      'http'
+    end
+  end
+  @@schemes['QRL'] = QRL
 end
 
 module SQRL
@@ -19,17 +36,21 @@ module SQRL
       URI::Parser.new(:UNRESERVED => "|\\-_.!~*'()a-zA-Z\\d")
     end
 
-    def initialize(domain_path, nut = nil)
-      if (!nut)
-        return super(domain_path)
-      end
+    def self.sqrl(domain_path, nut)
+      create(URI::SQRL, domain_path, nut)
+    end
 
+    def self.qrl(domain_path, nut)
+      create(URI::QRL, domain_path, nut)
+    end
+
+    def self.create(kind, domain_path, nut)
       parts = domain_path.split('/')
       host = parts.first
       parts[0] = ''
       path = parts.join('/')
       query = 'nut='+nut
-      super(URI::SQRL.new('sqrl', nil, host, nil, nil, path, nil, query, nil, self.class.parser))
+      new(kind.new(kind.scheme, nil, host, nil, nil, path, nil, query, nil, parser))
     end
 
     def nut
@@ -47,7 +68,7 @@ module SQRL
 
     def post_path
       path = dup
-      path.scheme = 'https'
+      path.scheme = post_scheme
       path.to_s.sub('|', '/')
     end
   end
--- spec/lib/sqrl/url_spec.rb
+++ spec/lib/sqrl/url_spec.rb
@@ -3,12 +3,18 @@ require 'sqrl/url'
 
 describe SQRL::URL do
   describe 'server' do
-    subject {SQRL::URL.new('example.com/sqrl', 'awnuts')}
+    subject {SQRL::URL.sqrl('example.com/sqrl', 'awnuts')}
     it {expect(subject.to_s).to eq('sqrl://example.com/sqrl?nut=awnuts')}
     it {expect(subject.post_path).to eq('https://example.com/sqrl?nut=awnuts')}
 
+    describe 'qrl' do
+      subject {SQRL::URL.qrl('example.com/sqrl', 'awnuts')}
+      it {expect(subject.to_s).to eq('qrl://example.com/sqrl?nut=awnuts')}
+      it {expect(subject.post_path).to eq('http://example.com/sqrl?nut=awnuts')}
+    end
+
     describe 'bar' do
-      subject {SQRL::URL.new('example.com/foo|sqrl', 'awnuts')}
+      subject {SQRL::URL.sqrl('example.com/foo|sqrl', 'awnuts')}
       it {expect(subject.to_s).to eq('sqrl://example.com/foo|sqrl?nut=awnuts')}
       it {expect(subject.post_path).to eq('https://example.com/foo/sqrl?nut=awnuts')}
     end
@@ -18,6 +24,12 @@ describe SQRL::URL do
     subject {SQRL::URL.parse('sqrl://example.com/sqrl?nut=awnuts')}
     it {expect(subject.signing_host).to eq('example.com')}
     it {expect(subject.nut).to eq('awnuts')}
+
+    describe 'qrl' do
+      subject {SQRL::URL.parse('qrl://example.com/sqrl?nut=awnuts')}
+      it {expect(subject.signing_host).to eq('example.com')}
+      it {expect(subject.nut).to eq('awnuts')}
+    end
 
     describe 'bar' do
       subject {SQRL::URL.parse('sqrl://example.com/foo|sqrl?nut=awnuts')}
